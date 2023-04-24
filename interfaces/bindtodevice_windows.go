4a5e568276a3cf38b105613e653a3ed6e2b72aa9
Andrew <andrew@paon.wtf>
2023-04-24 09:45:15 -0700

    Clarify BindToInterface error for windows
    Windows does not support binding sockets to a particular interface.
    
    There are many SO threads on this topic, e.g.
    https://stackoverflow.com/questions/2065495/using-a-specific-network-interface-for-a-socket-in-windows

--- interfaces/bindtodevice_windows.go
+++ interfaces/bindtodevice_windows.go
@@ -4,5 +4,5 @@ import "errors"
 
 // BindToInterface fails on Windows.
 func BindToInterface(fd int, ifname string) error {
-	return errors.New("not implemented on Windows")
+	return errors.New("SO_BINDTODEVICE is not supported on windows")
 }
